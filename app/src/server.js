ff582e59715a1c5ed6e638b0c1f329704c6a9725
Andrew Peterson <16apeterson@gmail.com>
2021-05-01 18:53:17 -0500

    chore: removed logging

--- app/src/server.js
+++ app/src/server.js
@@ -22,24 +22,14 @@ const CONTENT_TYPE_HEADER = {
 
 app.get('/*', (req, res) => {
 	let url = _url.parse(req.url).pathname.replace('/', '');
-	console.log(`pre-url: ${url}`);
 	let ext = path.extname(url).slice(1);
-	console.log(`ext: ${ext}`);
 	if (ext === '') {
 		ext = 'html';
 		url = '/index.html';
 	}
 
-	console.log(`final-url: ${url}`);
-	console.log(`ext: ${ext}`);
-	console.log(`${process.cwd()}/dist/${url}`);
-
 	fs.readFile(`${process.cwd()}/dist/${url}`, (err, data) => {	// eslint-disable-line
 		if (err) {
-			console.log(err);
-			console.log(`can't find shit yo`);
-			console.log(url);
-			console.log(`${process.cwd()}/dist/${url}`);
 			res.writeHead(404);
 			res.end("can't find shit yo");
 		} else {
